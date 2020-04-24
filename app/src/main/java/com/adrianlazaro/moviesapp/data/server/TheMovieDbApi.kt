91d0c6664a9758205b464e65e37da3d07e11b31e
Adrián Lázaro <adri92css@gmai.com>
2020-04-25 00:56:48 +0200

    Added API interface and server model

---
+++ app/src/main/java/com/adrianlazaro/moviesapp/data/server/TheMovieDbApi.kt
@@ -0,0 +1,13 @@
+package com.adrianlazaro.moviesapp.data.server
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TheMovieDbApi {
+
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): MovieDbDto
+}
