69b75cdc32d99b759f80b3dde812ea94e61581f5
Adrián Lázaro <adri92css@gmai.com>
2020-04-27 22:46:01 +0200

    View for detailMovie added

---
+++ app/src/main/java/com/adrianlazaro/moviesapp/ui/detail/MovieDetailInfoView.kt
@@ -0,0 +1,32 @@
+package com.adrianlazaro.moviesapp.ui.detail
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.adrianlazaro.domain.Movie
+
+class MovieDetailInfoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    fun setMovie(movie: Movie) = with(movie) {
+        text = buildSpannedString {
+
+            bold { append("Original language: ") }
+            appendln(originalLanguage)
+
+            bold { append("Original title: ") }
+            appendln(originalTitle)
+
+            bold { append("Release date: ") }
+            appendln(releaseDate)
+
+            bold { append("Popularity: ") }
+            appendln(popularity.toString())
+        }
+    }
+}
